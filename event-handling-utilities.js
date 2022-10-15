89fe243001a98ef1b32aca6ad50322e0cc98b40d
Sangeethakannas <8166067+SangeethaKannas@users.noreply.github.com>
2022-10-15 11:20:57 +0530

    Create event-handling-utilities.js

---
+++ event-handling-utilities.js
@@ -0,0 +1,15 @@
+"onCopy", (function(e) {
+      e.preventDefault()
+  }
+
+           
+  "onPaste", (function(e) {
+                    e.preventDefault()
+                }
+   
+
+
+ "onKeyUp", (function(e) {
+                    return 13 === e.keyCode ? //Enter
+                    !1) : 8 !== e.keyCode // Bakcspace
+                    !1))
